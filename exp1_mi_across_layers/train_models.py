c00a07b656f1f08c6d5b6408a6ddbafbc84958da
Johanmkr <johan.m.kroken@gmail.com>
2025-10-09 15:20:42 +0200

    Starting a concrete experiment

---
+++ exp1_mi_across_layers/train_models.py
@@ -0,0 +1,21 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import os, sys
+# Get the parent directory
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+# Add parent directory to sys.path
+sys.path.append(parent_dir)
+from src import treenode as tn
+from src import utils
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    pass
