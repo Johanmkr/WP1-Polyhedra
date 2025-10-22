22a9b91192fdbd133469f3a9a5b4198a4f804a9f
Johanmkr <johan.m.kroken@gmail.com>
2025-10-22 11:11:37 +0200

    testing funcionality

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
-from arg_parser import get_args, createfolders
+import src_experiment as scf
 import geobin as gb 
 import pathlib as pl
+import torch
+from sklearn.datasets import make_moons
+
+
+training_seed = 42
+testing_seed = 41
+inference_seed = 40
+
+
 
 # Read config file and/or parse cmb-line arguments
-args = get_args()
+args = scf.get_args()
 
-# Make sure the paths used exists and are valid
-current_path = pl.Path(__file__).parent.resolve()
-datapath = current_path / "stored_data"
+
 
 # Check if the experiment path exists, if not, create 
-experiment_path = datapath / args.experiment_name
+experiment_path = scf.get_path_to_experiment_storage(args.experiment_name)
 if not experiment_path.exists():
     createfolders(experiment_path)
+
+
+# Make the data
+train_data = scf.make_moon_dataloader()
 
 # Set up and train the model
 from IPython import embed; embed()
