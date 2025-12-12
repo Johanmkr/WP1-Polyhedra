c7c6907a8a4568bcdfc1d4389e585b8814462ab5
Johanmkr <johan.m.kroken@gmail.com>
2025-12-12 15:53:07 +0100

    working on MI estimation method

--- geobin/mi_estimate.py
+++ geobin/mi_estimate.py
@@ -7,13 +7,17 @@ import pathlib as pl
 import torch
 from sklearn.datasets import make_moons
 import matplotlib.pyplot as plt
-import pickle
+try:
+    import cpickle as pickle
+except ModuleNotFoundError:
+    import pickle
 import torch
 import os, sys
 from .region_tree import RegionTree
 from .tree_node import TreeNode
 import pandas as pd
 from collections import defaultdict
+from src_experiment import get_path_to_moon_experiment_storage
 
 def layer_wise_MI_from_number_counts(counts:pd.DataFrame):
     # See if dataframe has a column "total"
@@ -73,6 +77,10 @@ class EstimateQuantities1Run:
         self.noise_level = noise_level
         self.run_number = run_number
         
+        # # New variables
+        # self.num_layers = None
+        # self.classes = None
+        
         # Step 1 - Load the ncounts dictionary and create list of epochs
         
         # Step 2 - For each epoch do:
@@ -84,6 +92,85 @@ class EstimateQuantities1Run:
         # Step 3 - Make a layer-wise dictionary with the quantites per epoch, ready for plotting. 
         
         # Step 4 - Something else?
+        
+        
+        # 1:
+        # Path to data
+        self.data_path = get_path_to_moon_experiment_storage(model_name, dataset_name, noise_level, run_number)
+        
+        # Get number counts
+        self.ncounts = self._open_object(self.data_path) # Dict with epochs as keys
+        
+        # Get list of epoch
+        self.epochs = np.array(list(self.ncounts.keys()))
+        
+        estimates = {}
+        
+        # 2 - Iterate over all epochs
+        for epoch, frame in self.ncounts.items():
+            # Sort frame by layer idx
+            frame = frame.sort_values(by=["layer_idx", "region_idx"]).reset_index(drop=True)
+            
+            # Run consistency
+            frame, num_layers, classes, data_per_class = self._run_consistency_check_on_single_frame(frame)
+            
+            # Estimate quantites
+            estimates_per_layer = pd.DataFrame()
+            frame["MI_KL"] = 0.0
+            N = sum(data_per_class.values()) # Total number of points
+            n_w = np.array(frame["total"]) # Total number of points pr. region
+            for cl in classes: 
+                n_kw = np.array(frame[cl]) # Number of points pr. class pr. region
+                n_k = np.array(data_per_class[cl]) # Total number of points pr. class
+                
+                #### MI KL ####
+                # Two-step log to avoid 0 as argument
+                logterm = N*n_kw / (n_k*n_w)
+                logterm = np.log(logterm, where=logterm>0)
+                frame["MI_KL"] += n_kw / N * logterm
+            
+            for layer in range(1, num_layers+1):
+                estimates_per_layer["layer"] = layer
+                estimates_per_layer["MI_KL"] = sum(frame[frame.layer_idx==layer]["MI_KL"])
+                
+            
+            
+        
+        
+    def _run_consistency_check_on_single_frame(self, frame):
+        # Check if last column is "total" and check if the sum of total counts is the same for each layer.
+        assert frame.columns[ -1 ] == "total", "Last column should be 'total'"
+
+        # Find the class columns as the third to the second last columns
+        class_columns = frame.columns[ 2 : -1 ]
+        classes = class_columns.tolist()
+
+        # Check that the horisontal sum of classes equals the total counts for each row
+        # Find sum of all classes in one single row (region)
+        row_sums = frame[classes].sum(axis=1)
+        totals = frame["total"]
+        # Check if all row sums equal total counts
+        assert all(row_sums == totals), "Row sums of class counts should equal total counts"
+
+        # Find number of unique layers
+        num_layers = frame["layer_idx"].nunique()
+        # Get total counts per layer
+        total_counts_per_layer = frame.groupby("layer_idx")["total"].sum().values
+        # Check if all total counts are the same
+        assert all(total_counts_per_layer == total_counts_per_layer[0]), "Total counts should be the same for each layer"
+        
+        # Find data per class
+        layer_counts = frame[frame.layer_idx == 0]
+        data_per_class = {cl: np.sum(layer_counts[cl]) for cl in classes}
+
+        return frame, num_layers, classes, data_per_class
+    
+    
+    # Method to open and read pickled ojects. 
+    def _open_object(self, filename):
+        with open(filename, 'rb') as inp:
+            obj = pickle.load(inp)
+        return obj
 
 if __name__ == "__main__":
     pass
