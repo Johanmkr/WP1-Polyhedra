ef131f08e1dc1425783fd2f5ebff2b39e1e0229d
Johanmkr <johan.m.kroken@gmail.com>
2025-12-17 10:02:30 +0100

    code to visualize latent representations

--- src_experiment/__init__.py
+++ src_experiment/__init__.py
@@ -1,6 +1,6 @@
 __all__ = [
     "get_args", 
-    "NeuralNet", "moons_models", "paths", "createfolders", "train_model", "get_specific_moon_state_dict", "get_df_of_run_summary", "get_df_of_convergence_summary", "get_moon_dataloaders", "get_new_moons_data", "get_new_moons_data_for_all_noises", "get_model"]
+    "NeuralNet", "moons_models", "paths", "createfolders", "train_model", "get_specific_moon_state_dict", "get_df_of_run_summary", "get_df_of_convergence_summary", "get_moon_dataloaders", "get_new_moons_data", "get_new_moons_data_for_all_noises", "get_model", "VisualisationOfLatenRepresentation"]
 
 from .arg_parser import get_args
 from .utils import NeuralNet, createfolders, get_specific_moon_state_dict, get_df_of_run_summary, get_df_of_convergence_summary
@@ -9,3 +9,4 @@ from .dataset import get_new_moons_data, get_new_moons_data_for_all_noises
 
 from .train_models import train_model  
 from .models import moons_models, get_model
+from .visualize_data_representation import VisualisationOfLatenRepresentation
