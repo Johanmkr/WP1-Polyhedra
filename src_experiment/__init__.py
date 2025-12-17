01f65fdc98e092e187852b30ea8065bd762fa564
Johanmkr <johan.m.kroken@gmail.com>
2025-12-17 13:25:32 +0100

    Deleted old files, renamed and cleaned up main repo

--- src_experiment/__init__.py
+++ src_experiment/__init__.py
@@ -1,12 +1,65 @@
-__all__ = [
-    "get_args", 
-    "NeuralNet", "moons_models", "paths", "createfolders", "train_model", "get_specific_moon_state_dict", "get_df_of_run_summary", "get_df_of_convergence_summary", "get_moon_dataloaders", "get_new_moons_data", "get_new_moons_data_for_all_noises", "get_model", "VisualisationOfLatenRepresentation"]
+# __all__ = [
+#     "get_args", 
+#     "NeuralNet", "moons_models", "paths", "createfolders", "train_model", "get_specific_moon_state_dict", "get_df_of_run_summary", "get_df_of_convergence_summary", "get_moon_dataloaders", "get_new_moons_data", "get_new_moons_data_for_all_noises", "get_model", "VisualisationOfLatenRepresentation"]
 
-from .arg_parser import get_args
-from .utils import NeuralNet, createfolders, get_specific_moon_state_dict, get_df_of_run_summary, get_df_of_convergence_summary
-from .paths import *
-from .dataset import get_new_moons_data, get_new_moons_data_for_all_noises
+# from .arg_parser import get_args
+# from .utils import NeuralNet, createfolders, get_specific_moon_state_dict, get_df_of_run_summary, get_df_of_convergence_summary
+# from .paths import *
+# from .dataset import get_new_moons_data, get_new_moons_data_for_all_noises
 
-from .train_models import train_model  
-from .models import moons_models, get_model
-from .visualize_data_representation import VisualisationOfLatenRepresentation
+# from .train_models import train_model  
+# from .models import moons_models, get_model
+# from .visualize_data_representation import VisualisationOfLatenRepresentation
+
+
+
+# mypackage/__init__.py
+
+# Mapping of public names to their source modules
+_lazy_attributes = {
+    # Argument parser
+    "get_args": ".arg_parser",
+
+    # Models
+    "moons_models": ".models",
+    "get_model": ".models",
+    "NeuralNet": ".utils",
+
+    # Dataset
+    "get_new_moons_data": ".dataset",
+    "get_new_moons_data_for_all_noises": ".dataset",
+
+    # Training
+    "train_model": ".train_models",
+
+    # Utilities
+    "createfolders": ".utils",
+    "get_specific_moon_state_dict": ".utils",
+    "get_df_of_run_summary": ".utils",
+    "get_df_of_convergence_summary": ".utils",
+
+    # Visualization
+    "VisualisationOfLatenRepresentation": ".visualize_data_representation",
+}
+
+# Dynamically define __all__ based on lazy attributes
+__all__ = list(_lazy_attributes.keys())
+
+
+def __getattr__(name):
+    """
+    Lazily import attributes from submodules on access.
+    """
+    if name in _lazy_attributes:
+        module = __import__(_lazy_attributes[name], globals(), locals(), [name])
+        value = getattr(module, name)
+        globals()[name] = value  # cache in module namespace
+        return value
+    raise AttributeError(f"module {__name__} has no attribute {name}")
+
+
+def __dir__():
+    """
+    Include lazily-loaded attributes in dir() calls.
+    """
+    return sorted(list(globals().keys()) + list(_lazy_attributes.keys()))
--- train_models.py
+++ train_models.py
@@ -1,36 +1,5 @@
-"""Basic outline of script
-
-1. Load datasets and models. 
-2. For each model, dataset and noise level:
-    0. Create the folder to store the state dicts if it does not exist. This is done by the training loop only if the model should be saved. 
-    a. Train the model on the training data.
-    b. Evaluate the model on the test data.
-    c. Check if the model converges
-    e. Save the state dicts if it converges. 
-    
-savepath must have the following structure:
-
-./stored_data/
-    experiment_name/
-        model_name/
-            dataset_name/
-                noise_level/
-                    randomized_run_nr/
-                        state_dicts/
-                            somestate.pth   || This is the savepath argument
-                        run_summary.csv
-                        
-                    
-model_names = ["small_uniform", "small_uniform_long", "medium_uniform", "large_uniform", "increasing", "decreasing"]
-
-dataset_names = ["small", "medium", "large"]
-
-noise_levels = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-"""
-
 
 # Import necessary libraries
-from tqdm import trange
 import numpy as np
 from src_experiment import train_model, get_new_moons_data_for_all_noises, get_model
 from src_experiment.paths import get_test_moon_path
