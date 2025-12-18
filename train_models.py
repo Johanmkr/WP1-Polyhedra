36482fa5ad10a903798f3092a2d8e9574760e04c
Johanmkr <johan.m.kroken@gmail.com>
2025-12-18 15:51:56 +0100

    Adapted to fit new structure!

--- train_models.py
+++ train_models.py
@@ -1,39 +1,38 @@
 
 # Import necessary libraries
 import numpy as np
-from src_experiment import train_model, get_new_moons_data_for_all_noises, get_model
-from src_experiment.paths import get_test_moon_path
-
-# GLOABL PARAMETERS
-SAVE_STATES = True
-RETURN_STATES = False
-save_everyth_epoch = 5
-EPOCHS = 125
-
-train_data = get_new_moons_data_for_all_noises(type="training")
-test_data = get_new_moons_data_for_all_noises(type="testing")
 
 
-def train_single_model_on_moons(
+# Local imports
+from src_experiment import get_storage_path, get_model, train_model, get_data
+
+
+
+
+def train_model_on_moons(
     model_name: str,
     dataset_name: str,
     noise_level: float,
-    run_number: int = 0
+    run_number: int = 0,
+    epochs = 125,
+    SAVE_STATES = False,
+    RETURN_STATES = True,
+    save_everyth_epoch = 5,
 ):
     # Create savepath
-    savepath = get_test_moon_path(model_name=model_name, dataset_name=dataset_name, noise_level=noise_level, run_number=run_number)
+    savepath = get_storage_path("moons", model_name=model_name, dataset_name=dataset_name, noise_level=noise_level, run_number=run_number)
     
     # Get data
-    train_loader = train_data[noise_level]
-    test_loader = test_data[noise_level]
-    
+    train_loader = get_data("moons", "training", noise=noise_level)
+    test_loader = get_data("moons", "testing", noise=noise_level)
+   
     
     # Train model
     run_results = train_model(
         model=get_model(model_name, seed=run_number),
         train_data=train_loader,
         test_data=test_loader,
-        epochs=EPOCHS,
+        epochs=epochs,
         savepath=savepath,
         SAVE_STATES=SAVE_STATES,
         RETURN_STATES=RETURN_STATES,
@@ -46,12 +45,12 @@ def main():
     for noise in [0.05, 0.1, 0.15, 0.2, 0.3, 0.5]:
         for run_number in np.arange(35):
             print(f"Noise: {noise}\nRun {run_number}/{np.arange(35).max()}")
-            train_single_model_on_moons("decreasing", "new", noise, run_number=int(run_number))
+            train_model_on_moons("decreasing", "new", noise, run_number=int(run_number))
     
 def test():
-    # train_single_model_on_moons("decreasing", "new", 0.05, run_number=int(np.array([0])))
+    train_model_on_moons("decreasing", "new", 0.05, run_number=int(np.array([0])))
     pass
 
 if __name__ == "__main__":
     # main()
-    main()
+    test()
