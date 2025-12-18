466a95b6e4c9fc9c450e73402c12338b22661197
Johanmkr <johan.m.kroken@gmail.com>
2025-12-18 13:36:00 +0100

    minor

--- make_counts.py
+++ make_counts.py
@@ -98,10 +98,7 @@ def find_and_store_counts(model_name: str,
     else:
         print("Finding number counts...")
         _run_and_save_counts()
-    
-    
 
-    
     
 def test():
     run_numbers = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34]
